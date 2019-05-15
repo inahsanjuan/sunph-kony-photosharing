bc003f1d1da1c8326db3e3f9bb35e848284225ff
Inah SanJuan <inahsanjuan@gmail.com>
2019-05-15 17:52:36 +0800

    Main layout

--- controllers/mobile/FrmRegisterController/FrmRegisterControllerActions.js
+++ controllers/mobile/FrmRegisterController/FrmRegisterControllerActions.js
@@ -1,7 +1,11 @@
-define({ 
-
- /* 
- This is an auto generated file and any modifications to it may result in corruption of the action sequence.
- */
-
+define({
+    /*
+      This is an auto generated file and any modifications to it may result in corruption of the action sequence.
+    */
+    /** ontouchend defined for imgLogo **/
+    AS_Image_j803f22208744eb8bc61f942030e580d: function AS_Image_j803f22208744eb8bc61f942030e580d(eventobject, x, y) {
+        var self = this;
+        var ntf = new kony.mvc.Navigation("FrmFeed");
+        ntf.navigate();
+    }
 });
