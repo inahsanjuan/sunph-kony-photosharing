aae15278d3746d172168d55b133081cb3340779c
Inah SanJuan <inahsanjuan@gmail.com>
2019-05-17 11:39:47 +0800

    Navigation and current selected menuu

---
+++ actions/common/AS_FlexContainer_c742f84cfb5e42e0b6fa6a362db8f402.js
@@ -0,0 +1,4 @@
+function AS_FlexContainer_c742f84cfb5e42e0b6fa6a362db8f402(eventobject) {
+    var self = this;
+    new kony.mvc.Navigation("FrmFeed").navigate();
+}
--- userwidgets/Main.Navigation/modules/require/NavigationControllerActions.js
+++ userwidgets/Main.Navigation/modules/require/NavigationControllerActions.js
@@ -2,4 +2,14 @@ define({
     /*
       This is an auto generated file and any modifications to it may result in corruption of the action sequence.
     */
+    /** onClick defined for flxProfile **/
+    AS_FlexContainer_hfddf4ff2e244c739948914958d2c8b2: function AS_FlexContainer_hfddf4ff2e244c739948914958d2c8b2(eventobject) {
+        var self = this;
+        new kony.mvc.Navigation("FrmUserProfile").navigate();
+    },
+    /** onClick defined for flxFeed **/
+    AS_FlexContainer_c742f84cfb5e42e0b6fa6a362db8f402: function AS_FlexContainer_c742f84cfb5e42e0b6fa6a362db8f402(eventobject) {
+        var self = this;
+        new kony.mvc.Navigation("FrmFeed").navigate();
+    }
 });
